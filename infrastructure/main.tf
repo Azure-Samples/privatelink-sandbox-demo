7885a54b92783299e34a47755d8668250702dabd
Brian <brian@briandenicola.com>
2023-05-10 19:07:13 +0000

    Update flux_repo

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,10 +1,6 @@
 data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 
-data "http" "myip" {
-  url = "http://checkip.amazonaws.com/"
-}
-
 resource "random_id" "this" {
   byte_length = 2
 }
@@ -12,11 +8,6 @@ resource "random_id" "this" {
 resource "random_pet" "this" {
   length    = 1
   separator = ""
-}
-
-resource "random_password" "password" {
-  length  = 25
-  special = true
 }
 
 resource "random_integer" "vnet_cidr" {
@@ -48,7 +39,7 @@ locals {
   acr_account_name        = "${random_pet.this.id}${random_id.this.dec}acr"
   kv_name                 = "${local.resource_name}-kv"
   cluster_path            = "./cluster-config"
-  flux_repository         = "https://github.com/briandenicola/privatelink-service-demo"
+  flux_repository         = "https://github.com/azure-samples/privatelink-sandbox-demo"
   vnet_cidr               = cidrsubnet("10.0.0.0/8", 8, random_integer.vnet_cidr.result)
   fw_subnet_cidr          = cidrsubnet(local.vnet_cidr, 8, 0)
   pe_subnet_cidir         = cidrsubnet(local.vnet_cidr, 8, 1)
