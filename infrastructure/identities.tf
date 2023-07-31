d7d94c50ef6424c1f475ca0475e558c0020a0052
briandenicola <brian@briandenicola.com>
2023-07-31 09:46:34 -0500

    Added federation

--- infrastructure/identities.tf
+++ infrastructure/identities.tf
@@ -11,7 +11,16 @@ resource "azurerm_user_assigned_identity" "aks_kubelet_identity" {
 }
 
 resource "azurerm_user_assigned_identity" "aks_istio_identity" {
-  name                = "${local.aks_name}-istio-identity"
+  name                = local.ingress_identity
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
+}
+
+resource "azurerm_federated_identity_credential" "aks_service_mesh_ingress_identity" {
+  name                = local.ingress_identity
+  resource_group_name = azurerm_resource_group.this.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = azurerm_kubernetes_cluster.aks.oidc_issuer_url
+  parent_id           = azurerm_user_assigned_identity.aks_istio_identity.id
+  subject             = "system:serviceaccount:${var.ingress_namespace}:${local.ingress_identity}"
 }
