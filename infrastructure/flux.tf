9836b02518cc551649e4b8cc042769845d7e7590
briandenicola <brian@briandenicola.com>
2023-07-31 09:00:26 -0500

    Moved to azurerm_kubernetes_flux_configuration

--- infrastructure/flux.tf
+++ infrastructure/flux.tf
@@ -1,45 +1,37 @@
 resource "azurerm_kubernetes_cluster_extension" "flux" {
   depends_on = [
-    azapi_update_resource.istio_ingressgateway
+    #azapi_update_resource.updates
+    azurerm_kubernetes_cluster.aks
   ]
   name           = "flux"
   cluster_id     = azurerm_kubernetes_cluster.aks.id
   extension_type = "microsoft.flux"
 }
 
-
-resource "azapi_resource" "flux_config" {
+resource "azurerm_kubernetes_flux_configuration" "flux_config" {
   depends_on = [
     azurerm_kubernetes_cluster_extension.flux
   ]
 
-  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2022-03-01"
-  name      = "aks-flux-extension"
-  parent_id = azurerm_kubernetes_cluster.aks.id
+  name       = "aks-flux-extension"
+  cluster_id = azurerm_kubernetes_cluster.aks.id
+  namespace  = "flux-system"
+  scope      = "cluster"
 
-  body = jsonencode({
-    properties : {
-      scope      = "cluster"
-      namespace  = "flux-system"
-      sourceKind = "GitRepository"
-      suspend    = false
-      gitRepository = {
-        url                   = local.flux_repository
-        timeoutInSeconds      = 600
-        syncIntervalInSeconds = 300
-        repositoryRef = {
-          branch = "main"
-        }
-      }
-      kustomizations : {
-        apps = {
-          path                   = local.cluster_path
-          timeoutInSeconds       = 600
-          syncIntervalInSeconds  = 120
-          retryIntervalInSeconds = 300
-          prune                  = true
-        }
-      }
-    }
-  })
+  git_repository {
+    url                      = local.flux_repository
+    reference_type           = "branch"
+    reference_value          = "main"
+    timeout_in_seconds       = 600
+    sync_interval_in_seconds = 30
+  }
+
+  kustomizations {
+    name                       = "apps"
+    path                       = local.cluster_path
+    timeout_in_seconds         = 600
+    sync_interval_in_seconds   = 120
+    retry_interval_in_seconds  = 300
+    garbage_collection_enabled = true
+  }
 }
