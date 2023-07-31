04122af613600ccca174b5ef8a1bd0b541aaf4dd
briandenicola <brian@briandenicola.com>
2023-07-31 09:00:37 -0500

    Added azurerm_key_vault_certificate

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -46,3 +46,47 @@ resource "azurerm_private_endpoint" "key_vault" {
     private_dns_zone_ids          = [ azurerm_private_dns_zone.privatelink_vaultcore_azure_net.id ]
   }
 }
+
+resource "azurerm_key_vault_certificate" "this" {
+  name         = "developer-certificate"
+  key_vault_id = azurerm_key_vault.this.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = false
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+    x509_certificate_properties {
+      extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
+
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject_alternative_names {
+        dns_names = [
+            "api.${local.resource_name}.local"
+        ]
+      }
+
+      subject            = "CN=*.${local.resource_name}.local"
+      validity_in_months = 12
+    }
+  }
+}
