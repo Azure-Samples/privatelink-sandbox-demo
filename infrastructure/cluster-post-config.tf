3b9681033bd745d353713dc04a7bafdec8b1a887
briandenicola <brian@briandenicola.com>
2023-07-31 09:02:18 -0500

    Moved to internal_ingress_gateway_enabled

--- infrastructure/cluster-post-config.tf
+++ infrastructure/cluster-post-config.tf
@@ -1,23 +1,11 @@
-resource "azapi_update_resource" "istio_ingressgateway" {
-  depends_on = [
-    azurerm_kubernetes_cluster.aks
-  ]
+# resource "azapi_update_resource" "updates" {
+#   depends_on = [
+#     azurerm_kubernetes_cluster.aks
+#   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2023-03-02-preview"
-  resource_id = azurerm_kubernetes_cluster.aks.id
+#   type        = "Microsoft.ContainerService/managedClusters@2023-03-02-preview"
+#   resource_id = azurerm_kubernetes_cluster.aks.id
 
-  body = jsonencode({
-    properties = {
-      serviceMeshProfile = {
-        istio = {
-          components = {
-            ingressGateways = [{
-              mode = "Internal",
-              enabled = true
-            }]
-          }
-        }
-      }
-    }
-  })
-}
+#   body = jsonencode({
+#   })
+# }
