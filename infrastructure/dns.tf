b56dc5965eae948f7bf67402844fa9c869946c7d
briandenicola <brian@briandenicola.com>
2023-08-23 13:08:53 -0500

    Bug fix

--- infrastructure/dns.tf
+++ infrastructure/dns.tf
@@ -59,12 +59,12 @@ resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_vaultcore_
 }
 
 resource "azurerm_private_dns_zone" "local_developer_zone" {
-  name                = "contoso.tech"
+  name                = "${local.resource_name}.local"
   resource_group_name = azurerm_resource_group.this.name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "local_developer_zone" {
-  name                  = "${azurerm_virtual_network.this.name}-contoso-link"
+  name                  = "${azurerm_virtual_network.this.name}-developer-link"
   private_dns_zone_name = azurerm_private_dns_zone.local_developer_zone.name
   resource_group_name   = azurerm_resource_group.this.name
   virtual_network_id    = azurerm_virtual_network.this.id
